6044bcd9bd40bac4dd041994cc825b17d5195a0b
jevgenir <zeka.rum@gmail.com>
2014-07-02 12:11:20 +0300

    Init

---
+++ Tests/RoleStoreTests.cs
@@ -0,0 +1,73 @@
+﻿using AspNet.Identity.Bltoolkit;
+using BLToolkit.Data;
+using BLToolkit.Data.Linq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class RoleStoreTests
+    {
+        public RoleStoreTests()
+        {
+            DbManager.AddDataProvider(new PostgreSQLDataProvider());
+        }
+
+        [Test]
+        public void UserIdentityCreateAndFindTest()
+        {
+            // Create
+            var roleStore = new RoleStore();
+            var role = new IdentityRole("Warrior");
+            roleStore.CreateAsync(role);
+
+            // Check by id, if it was created
+            var userFromDb1 = roleStore.FindByIdAsync(role.Id);
+            Assert.AreEqual("Warrior", userFromDb1.GetAwaiter().GetResult().Name);
+
+            var userFromDb2 = roleStore.FindByNameAsync(role.Name);
+            Assert.AreEqual("Warrior", userFromDb2.GetAwaiter().GetResult().Name);
+        }
+
+        [Test]
+        public void UserIdentityUpdate()
+        {
+            // Create
+            var roleStore = new RoleStore();
+            var role = new IdentityRole("Warrior");
+            roleStore.CreateAsync(role);
+
+            // Update
+            role.Name = "Wizard";
+            roleStore.UpdateAsync(role);
+
+            // Check, if it was updated
+            var userFromDb = roleStore.FindByIdAsync(role.Id);
+            Assert.AreEqual("Wizard", userFromDb.GetAwaiter().GetResult().Name);
+        }
+
+        [Test]
+        public void UserIdentityDelete()
+        {
+            // Create
+            var roleStore = new RoleStore();
+            var role = new IdentityRole("Warrior");
+            roleStore.CreateAsync(role);
+
+            // Delete
+            roleStore.DeleteAsync(role);
+
+            // Check, if it was deleted
+            var userFromDb = roleStore.FindByIdAsync(role.Id);
+            Assert.IsNull(userFromDb.GetAwaiter().GetResult());
+        }
+
+        [Test]
+        public void ClearData()
+        {
+            using (var db = new SaisDb())
+            {
+                db.Roles.Delete();
+            }
+        }
+    }
+}
---
+++ Tests/UserStoreTests.cs
@@ -0,0 +1,63 @@
+﻿using AspNet.Identity.Bltoolkit;
+using BLToolkit.Data;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class UserStoreTests
+    {
+        public UserStoreTests()
+        {
+            DbManager.AddDataProvider(new PostgreSQLDataProvider());
+        }
+
+        [Test]
+        public void UserIdentityCreateAndFindTest()
+        {
+            // Create
+            var userStore = new UserStore();
+            var user = new IdentityUser("John Snow");
+            userStore.CreateAsync(user);
+
+            // Check by id, if it was created
+            var userFromDb1 = userStore.FindByIdAsync(user.Id);
+            Assert.AreEqual("John Snow", userFromDb1.GetAwaiter().GetResult().UserName);
+
+            var userFromDb2 = userStore.FindByNameAsync(user.UserName);
+            Assert.AreEqual("John Snow", userFromDb2.GetAwaiter().GetResult().UserName);
+        }
+
+        [Test]
+        public void UserIdentityUpdate()
+        {
+            // Create
+            var userStore = new UserStore();
+            var user = new IdentityUser("John Snow");
+            userStore.CreateAsync(user);
+
+            // Update
+            user.UserName = "Ned Stark";
+            userStore.UpdateAsync(user);
+
+            // Check, if it was updated
+            var userFromDb = userStore.FindByIdAsync(user.Id);
+            Assert.AreEqual("Ned Stark", userFromDb.GetAwaiter().GetResult().UserName);
+        }
+
+        [Test]
+        public void UserIdentityDelete()
+        {
+            // Create
+            var userStore = new UserStore();
+            var user = new IdentityUser("John Snow");
+            userStore.CreateAsync(user);
+
+            // Delete
+            userStore.DeleteAsync(user);
+
+            // Check, if it was deleted
+            var userFromDb = userStore.FindByIdAsync(user.Id);
+            Assert.IsNull(userFromDb.GetAwaiter().GetResult());
+        }
+    }
+}
